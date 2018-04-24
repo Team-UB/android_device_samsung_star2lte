d25fb8814cdc0daab18e87d24939ddb0e7e2f2b4
abun880007 <abun880007@msn.com>
2018-04-24 14:22:07 +0100

    Add vendor Props and boot resolution
    # Conflicts:
    #	full_star2lte.mk
    
    # Conflicts:
    #	full_star2lte.mk
    
    # Conflicts:
    #	full_star2lte.mk

--- full_star2lte.mk
+++ full_star2lte.mk
@@ -26,6 +26,15 @@ TARGET_RELEASETOOLS_EXTENSIONS := $(COMMON_PATH)/releasetools
 # Boot animation
 TARGET_BOOT_ANIMATION_RES := 1440
 
-# PixelExperience stuff
+# TUB stuff
 TARGET_GAPPS_ARCH := arm64
 TARGET_DENSITY := xxhdpi
+
+# VendorDevice identifier.
+PRODUCT_PROPERTY_OVERRIDES := \
+ro.vendor.product.manufacturer=samsung \
+ro.vendor.product.model=SM-G965F \
+ro.vendor.product.brand=samsung \
+ro.vendor.product.name=tub_star2lte \
+ro.vendor.product.device=star2lte
+
