3bc8883edd30f43075c5e025a8751eea6d24653c
abun880007 <abun880007@msn.com>
2018-07-03 02:54:33 +0100

    add rr

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -2,3 +2,7 @@ add_lunch_combo tub_star2lte-userdebug
 add_lunch_combo tub_star2lte-user
 add_lunch_combo tub_star2lte-eng
 add_lunch_combo tub_star2lte
+add_lunch_combo rr_star2lte-userdebug
+add_lunch_combo rr_star2lte-user
+add_lunch_combo rr_star2lte-eng
+add_lunch_combo rr_star2lte
