74829177b429779b346e170bacf133e5cc86c9bb
abun880007 <abun880007@msn.com>
2019-02-26 18:51:20 +0000

    TUB Time
    # Conflicts:
    #	AndroidProducts.mk
    #	BoardConfig.mk
    #	tub_star2lte.mk
    
    # Conflicts:
    #	AndroidProducts.mk
    #	BoardConfig.mk

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -8,7 +8,7 @@ TARGET_OTA_ASSERT_DEVICE := star2ltexx,star2lteks,star2lte
 
 # Kernel
 TARGET_KERNEL_CONFIG := exynos9810-star2lte_defconfig
-TARGET_KERNEL_SOURCE := kernel/samsung/universal9810-star
+TARGET_KERNEL_SOURCE := kernel/samsung/universal9810
 
 # Bluetooth
 BOARD_BLUETOOTH_BDROID_BUILDCFG_INCLUDE_DIR := $(DEVICE_PATH)/bluetooth
@@ -18,9 +18,6 @@ include device/samsung/universal9810-common/BoardConfigCommon.mk
 
 # Use mke2fs to create ext4 images
 TARGET_USES_MKE2FS := true
-
-# Releasetools
-TARGET_RELEASETOOLS_EXTENSIONS := $(DEVICE_PATH)/releasetools
 
 # Set default locale
 PRODUCT_LOCALES := en-GB
---
+++ vendorsetup.sh
@@ -0,0 +1,12 @@
+add_lunch_combo tub_star2lte-userdebug
+add_lunch_combo tub_star2lte-user
+add_lunch_combo tub_star2lte-eng
+add_lunch_combo tub_star2lte
+add_lunch_combo rr_star2lte-userdebug
+add_lunch_combo rr_star2lte-user
+add_lunch_combo rr_star2lte-eng
+add_lunch_combo rr_star2lte
+add_lunch_combo aosp_star2lte-userdebug
+add_lunch_combo aosp_star2lte-user
+add_lunch_combo aosp_star2lte-eng
+add_lunch_combo aosp_star2lte
