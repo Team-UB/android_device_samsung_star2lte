35b8652e008c8eacc3b7215e53458097f24bba22
abun880007 <abun880007@msn.com>
2019-08-27 17:41:35 +0100

    more fixes

--- system_prop.mk
+++ system_prop.mk
@@ -4,3 +4,21 @@
 
 PRODUCT_PROPERTY_OVERRIDES += \
     ro.sf.lcd_density=420
+
+# Enahnced 4g tweak
+persist.dbg.ims_volte_enable=1
+persist.dbg.volte_avail_ovr=1
+persist.dbg.vt_avail_ovr=1
+persist.dbg.wfc_avail_ovr=1
+persist.ims.volte=true
+persist.volte_enabled_by_hw=1
+net.lte.volte_call_capable=true
+persist.radio.calls.on.ims=true
+persist.radio.csvt.enabled=false
+persist.radio.jbims=1
+persist.radio.ROTATION_ENABLE=1
+persist.radio.VT_ENABLE=1
+persist.radio.VT_HYBRID_ENABLE=1
+persist.radio.VT_USE_MDM_TIME=0
+persist.radio.vrte_logic=1
+persist.radio.domain.ps=true
